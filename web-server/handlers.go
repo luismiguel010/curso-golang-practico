2480675d9541464def209def15fd1d2360bcdbdc
luismiguel010 <luismiguel010@gmail.com>
2021-09-04 20:33:39 -0500

    se añade un nuevo handler que recibe un json y devuelve el mismo json mapeándolo con un struct user

--- web-server/handlers.go
+++ web-server/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,32 @@ func HandlerRoot(w http.ResponseWriter, r *http.Request) {
 
 func HandleHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the API Endpoint")
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	decorder := json.NewDecoder(r.Body)
+	var metadata MetaData 
+	err := decorder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Payload %v\n", metadata)
+}
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	decorder := json.NewDecoder(r.Body)
+	var user User 
+	err := decorder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	response, err := user.ToJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
--- web-server/main.go
+++ web-server/main.go
@@ -3,6 +3,8 @@ package main
 func main() {
 	server := NewServer(":3000")
 	server.Handle("GET", "/", HandlerRoot)
+	server.Handle("POST", "/create", PostRequest)
+	server.Handle("POST", "/user", UserPostRequest)
 	server.Handle("POST", "/api", server.AddMiddleware(HandleHome, ChackAuth(), Logging()))
 	server.Listen()
 }
