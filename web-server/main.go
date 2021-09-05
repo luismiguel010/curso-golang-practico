fd00198ab8fbeb1b213241f5ed64a9236ed3c7d6
luismiguel010 <luismiguel010@gmail.com>
2021-09-04 19:43:24 -0500

    Se añaden middlewares y se añade control de tipos de métodos http

--- web-server/main.go
+++ web-server/main.go
@@ -2,7 +2,7 @@ package main
 
 func main() {
 	server := NewServer(":3000")
-	server.Handle("/", HandlerRoot)
-	server.Handle("/api", HandleHome)
+	server.Handle("GET", "/", HandlerRoot)
+	server.Handle("POST", "/api", server.AddMiddleware(HandleHome, ChackAuth(), Logging()))
 	server.Listen()
 }
--- web-server/server.go
+++ web-server/server.go
@@ -1,6 +1,8 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type Server struct{
 	port string
@@ -14,8 +16,19 @@ func NewServer(port string) *Server{
 	}
 }
 
-func (s *Server) Handle(path string, handler http.HandlerFunc) {
-	s.router.rules[path] = handler
+func (s *Server) Handle(method string, path string, handler http.HandlerFunc) {
+	_, exist := s.router.rules[path]
+	if !exist {
+		s.router.rules[path] = make(map[string]http.HandlerFunc)
+	}
+	s.router.rules[path][method] = handler
+}
+
+func (s *Server) AddMiddleware(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
 }
 
 func (s *Server) Listen() error {
